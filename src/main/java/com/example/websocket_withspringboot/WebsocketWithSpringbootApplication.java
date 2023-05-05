05548baeb69d1e0c608dab98030fff2aa09ab1a0
ifeanyichukwuobeta <angeloobeta@hotmail.com>
2023-05-05 14:03:01 +0100

    Initial setup

---
+++ src/main/java/com/example/websocket_withspringboot/WebsocketWithSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.websocket_withspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketWithSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketWithSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/websocket_withspringboot/WebsocketWithSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websocket_withspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketWithSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
