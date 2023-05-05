3072fa622c2345b64dbbf7c366b3006f5a11e51f
ifeanyichukwuobeta <angeloobeta@hotmail.com>
2023-05-05 15:39:04 +0100

    Added the static files for the front ed

--- src/main/java/com/example/websocket_withspringboot/controller/GreetingController.java
+++ src/main/java/com/example/websocket_withspringboot/controller/GreetingController.java
@@ -1,7 +1,17 @@
 package com.example.websocket_withspringboot.controller;
 
+import com.example.websocket_withspringboot.Greeting;
+import com.example.websocket_withspringboot.HelloMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
 
 @Controller
 public class GreetingController {
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greet(HelloMessage helloMessage) throws InterruptedException{
+        return new Greeting("Hello" + HtmlUtils.htmlEscape(helloMessage.getName()));
+    }
 }
